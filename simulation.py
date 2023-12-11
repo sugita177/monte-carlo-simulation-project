514024d60455a76bde846e4bba78f8428b6b8c42
sugita177 <moriaki.sugita.1730@gmail.com>
2023-12-11 12:15:32 +0900

    ボンド角ポテンシャルの途中

--- simulation.py
+++ simulation.py
@@ -6,12 +6,19 @@ import random
 import time
 
 
-def calc_potential(distance: float,
+def fene_potential(bond_length: float,
                    max_length: float,
-                   bond_length: float,
+                   natural_bond_length: float,
                    spring_constant: float) -> float:
     return -spring_constant/2 * (max_length - bond_length)**2\
-           * math.log(1 - ((distance - bond_length)/(max_length +10**(-3) - bond_length))**2)
+           * math.log(1 - ((bond_length - natural_bond_length)/(max_length +10**(-3) - natural_bond_length))**2)
+
+def morse_potential():
+    return 0
+
+def bond_angle_potential(bond_angle: float, bond_angle_potential_const: float) -> float:
+    return bond_angle_potential_const * (1.0 - math.cos(bond_angle))
+
 
 
 def get_initial_state(particles_number: int, max_length: float, bond_length: float):
@@ -24,22 +31,40 @@ def get_initial_state(particles_number: int, max_length: float, bond_length: flo
     return np.asarray(points_list)
 
 
-def proceed_mc_step(points, particles_number: int, inverse_temperature: float, max_length: float, bond_length: float, spring_constant: float):
+def proceed_mc_step(points, particles_number: int, inverse_temperature: float, max_length: float, natural_bond_length: float, spring_constant: float):
     i = random.randint(1, particles_number-1)
-    distance = random.uniform(-max_length + 2 * bond_length, max_length)
+    distance = random.uniform(-max_length + 2 * natural_bond_length, max_length)
     angle = random.uniform(0, math.pi * 2)
-    new_point = [points[i][0] + distance * math.cos(angle) * 10**(-1),
-                 points[i][1] + distance * math.sin(angle) * 10**(-1)]
-    old_distance_1 = math.sqrt((points[i][0] - points[i-1][0])**2 + (points[i][1] - points[i-1][1])**2)
-    new_distance_1 = math.sqrt((new_point[0] - points[i-1][0])**2 + (new_point[1] - points[i-1][1])**2)
-    old_distance_2 = math.sqrt((points[i][0] - points[i+1][0])**2 + (points[i][1] - points[i+1][1])**2)
-    new_distance_2 = math.sqrt((new_point[0] - points[i+1][0])**2 + (new_point[1] - points[i+1][1])**2)
-    if not (-max_length + 2*bond_length < new_distance_1 < max_length) or not (-max_length + 2*bond_length < new_distance_2 < max_length):
+    new_point = np.ndarray(
+        [points[i][0] + distance * math.cos(angle) * 10**(-1),
+         points[i][1] + distance * math.sin(angle) * 10**(-1)]
+    )
+    old_bond_vector_1 = np.ndarray([points[i][0] - points[i-1][0], points[i][1] - points[i-1][1]])
+    new_bond_vector_1 = np.ndarray([new_point[0] - points[i-1][0], new_point[1] - points[i-1][1]])
+    old_bond_vector_2 = np.ndarray([points[i+1][0] - points[i][0], points[i+1][1] - points[i][1]])
+    new_bond_vector_2 = np.ndarray([points[i+1][0] - new_point[0], points[i+1][1] - new_point[1]])
+
+    old_bond_length_1 = np.linalg.norm(old_bond_vector_1)
+    new_bond_length_1 = np.linalg.norm(new_bond_vector_1)
+    old_bond_length_2 = np.linalg.norm(old_bond_vector_2)
+    new_bond_length_2 = np.linalg.norm(new_bond_vector_2)
+
+    old_bond_angle = np.dot(old_bond_vector_1, old_bond_vector_2) / (old_bond_length_1 * old_bond_length_2)
+    new_bond_angle = np.dot(new_bond_vector_1, old_bond_vector_2) / (new_bond_length_1 * old_bond_length_2)
+
+
+    # old_bond_length_1 = math.sqrt((points[i][0] - points[i-1][0])**2 + (points[i][1] - points[i-1][1])**2)
+    # new_bond_length_1 = math.sqrt((new_point[0] - points[i-1][0])**2 + (new_point[1] - points[i-1][1])**2)
+    # old_bond_length_2 = math.sqrt((points[i][0] - points[i+1][0])**2 + (points[i][1] - points[i+1][1])**2)
+    # new_bond_length_2 = math.sqrt((new_point[0] - points[i+1][0])**2 + (new_point[1] - points[i+1][1])**2)
+    if not (-max_length + 2*natural_bond_length < new_bond_length_1 < max_length) or not (-max_length + 2*natural_bond_length < new_bond_length_2 < max_length):
         return points
-    diff_potential = + calc_potential(new_distance_1, max_length, bond_length, spring_constant)\
-                     - calc_potential(old_distance_1, max_length, bond_length, spring_constant)\
-                     + calc_potential(new_distance_2, max_length, bond_length, spring_constant)\
-                     - calc_potential(old_distance_2, max_length, bond_length, spring_constant)
+    diff_potential = + fene_potential(new_bond_length_1, max_length, natural_bond_length, spring_constant)\
+                     - fene_potential(old_bond_length_1, max_length, natural_bond_length, spring_constant)\
+                     + fene_potential(new_bond_length_2, max_length, natural_bond_length, spring_constant)\
+                     - fene_potential(old_bond_length_2, max_length, natural_bond_length, spring_constant)\
+                     + bond_angle_potential(new_bond_angle, )
+                     - bond_angle_potential(old_bond_angle, )
     if diff_potential <= 0:
         points[i] = new_point
     else:
@@ -62,7 +87,7 @@ def main():
     TEMPERATURE = 300
     BOLTZMAN_CONSTANT = 1.38 * 10**(-23)
     INVRESE_TEMPERATURE = 1 / (BOLTZMAN_CONSTANT * TEMPERATURE)
-    STEP_NUMBER = 10**4
+    STEP_NUMBER = 10**3
     PARTICLES_NUMBER = 50
     BOND_LENGTH = 1.0
     MAX_LENGTH = 1.2 * BOND_LENGTH
